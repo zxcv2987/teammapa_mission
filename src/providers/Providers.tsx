d4bcb42df3b216b9d080d102da14480f8fe2abac
bigsheep <zxcv2987@naver.com>
2025-06-19 17:55:28 +0900

    fix : queryProvider 네이밍 변경경

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,15 +1,5 @@
-'use client';
-import {QueryClientProvider} from '@tanstack/react-query';
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
-import {getQueryClient} from '@/lib/queryClient/getQueryClient';
-import type * as React from 'react';
+import QueryProvider from './QueryProvider';
 
 export default function Providers({children}: {children: React.ReactNode}) {
-  const queryClient = getQueryClient();
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 }

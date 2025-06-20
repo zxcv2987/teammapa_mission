8e4edd53cfc18fa8b608611e3964ce37cdabd991
bigsheep <zxcv2987@naver.com>
2025-06-20 12:33:29 +0900

    fix : 누락된 커밋 추가가

--- src/features/order/components/OrderList.tsx
+++ src/features/order/components/OrderList.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import {useOrderStore} from '@/features/order/store/orderStore';
+import {Button} from '@/ui/button';
+import {Trash2} from 'lucide-react';
 
 export default function OrderList() {
   const orderList = useOrderStore(state => state.items);
@@ -14,21 +16,25 @@ export default function OrderList() {
     );
   }
   return (
-    <div className="flex flex-col gap-4 p-4">
+    <div className="flex flex-col gap-4 p-4 flex-wrap whitespace-nowrap">
       {orderList.map(order => (
-        <button
-          key={order.menu.id}
-          className="flex flex-row gap-2"
-          onClick={() => removeItem(order.menu.id)}>
-          <div className="flex flex-row gap-2 justify-between w-full hover:bg-muted p-2 px-4 rounded-md">
+        <div key={order.menu.id} className="flex flex-row gap-2 items-center">
+          <span className="flex flex-row gap-2 justify-between w-full p-2 px-4 ">
             <h1 className="text-xl font-bold text-zinc-700">
               {order.menu.name}
             </h1>
             <span className="text-xl text-zinc-700">
               {`x ${order.quantity}`}
             </span>
-          </div>
-        </button>
+          </span>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => removeItem(order.menu.id)}
+            className="hover:text-red-500 cursor-pointer">
+            <Trash2 />
+          </Button>
+        </div>
       ))}
     </div>
   );
